7b7dddc74d3647a2c66aa25b0e11adff0906a465
manunez19 <marionunez19@hotmail.com>
2016-10-14 11:19:43 -0500

    Add files via upload

--- fotosf.php
+++ fotosf.php
@@ -2,6 +2,6 @@
 "http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1433832597046-4f10e10ac764.jpg",
 "http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1441906363162-903afd0d3d52.jpg",
 "http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1442120108414-42e7ea50d0b5.jpg",
-"https://www.flickr.com/photos/148038946@N05/30320952035/in/photostream/",
+"//c4.staticflickr.com/6/5503/30320952035_d0c35a0204.jpg",
 "http://www.kaleidosblog.com//tutorial//img//photo-1433832597046-4f10e10ac764.jpg"
 ]
