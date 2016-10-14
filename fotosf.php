d9dda90d07d1af2b7711f57e8761bf576c2f9fff
manunez19 <marionunez19@hotmail.com>
2016-10-14 11:11:31 -0500

    Update fotosf.php

--- fotosf.php
+++ fotosf.php
@@ -1,5 +1,6 @@
 [
 "http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1433832597046-4f10e10ac764.jpg",
 "http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1441906363162-903afd0d3d52.jpg",
-"http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1442120108414-42e7ea50d0b5.jpg"
+"http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1442120108414-42e7ea50d0b5.jpg",
+"https://www.flickr.com/photos/148038946@N05/30320952035/in/photostream/"
 ]
