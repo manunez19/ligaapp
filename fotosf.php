2bea986b3c2712f6d9f32b8572b68f856a36abed
manunez19 <marionunez19@hotmail.com>
2016-10-14 11:09:55 -0500

    Add files via upload

---
+++ fotosf.php
@@ -0,0 +1,5 @@
+[
+"http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1433832597046-4f10e10ac764.jpg",
+"http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1441906363162-903afd0d3d52.jpg",
+"http:\/\/kaleidosblog.com\/tutorial\/img\/photo-1442120108414-42e7ea50d0b5.jpg"
+]
